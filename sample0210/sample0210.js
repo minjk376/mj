50f37affc5a4b63c9ba555232f25a458ee859f50
minjk376 <158014660+minjk376@users.noreply.github.com>
2024-02-14 13:32:18 +0900

    d

--- sample0210/sample0210.js
+++ sample0210/sample0210.js
@@ -1,8 +1,7 @@
-// 아래 코드 상대 경로 설정에 문제가 있는 것 같은데.....
-// import { GLTFLoader } from "../applications-20230306/applications/libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js"
+// import { GLTFLoader } from "../applications-20230306/applications/libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js";
 
 import * as THREE from 'three';
-import {MindARThree} from 'mindar-image-three';
+import { MindARThree } from 'mindar-image-three';
 
 document.addEventListener('DOMContentLoaded', () => {
     const start = async () => {
@@ -12,22 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         const { renderer, scene, camera } = mindarThree;
 
-        // create AR object
+        // const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        // scene.add(light);
+
         const geometry = new THREE.PlaneGeometry(1, 1);
         const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, transparent: true, opacity: 0.5 });
         const plane = new THREE.Mesh(geometry, material);
 
-        // create anchor
         const anchor = mindarThree.addAnchor(0);
         anchor.group.add(plane);
 
         // const loader = new GLTFLoader();
         // loader.load("../applications-20230306/applications/assets/models/musicband-raccoon/scene.gltf", (gltf) => {
-        //     //gltf.scene: THREE.Group
+        //     // gltf.scene: THREE.Group
+        //     gltf.scene.scale.set(0.1, 0.1, 0.1);
+        //     gltf.scene.position.set(0,-0.4,0);
         //     anchor.group.add(gltf.scene);
         // });
 
-        // start AR
         await mindarThree.start();
         renderer.setAnimationLoop(() => {
             renderer.render(scene, camera);
