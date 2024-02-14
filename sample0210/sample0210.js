61bd2f4e5a350e74b566bd23d8953b5544592a12
minjk376 <158014660+minjk376@users.noreply.github.com>
2024-02-14 14:47:46 +0900

    F

--- sample0210/sample0210.js
+++ sample0210/sample0210.js
@@ -13,12 +13,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const raccoon = await loadGLTF('./scene.gltf');
-        raccoon.scene.scale.set(0.1, 0.1, 0.1);
-        raccoon.scene.position.set(0, -0.4, 0);
-        
         const anchor = mindarThree.addAnchor(0);
-        anchor.group.add(raccoon.scene);
+
+        const gltf = await loadGLTF("./scene.gltf");
+        gltf.scene.scale.set(0.1, 0.1, 0.1);
+        gltf.scene.position.set(0, -0.4, 0);
+        anchor.group.add(gltf.scene);
 
         await mindarThree.start();
         renderer.setAnimationLoop(() => {
